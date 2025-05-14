88430576f7285be0e07c3c3174ed18dbb65cbea7
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2025-05-15 03:54:46 +0900

    feat(AUTH-SERVICE): 배포용 yaml을 추가하고 dockerfile을 수정한다

--- api-gateway/src/main/java/guideme/apigateway/client/AuthServiceClient.java
+++ api-gateway/src/main/java/guideme/apigateway/client/AuthServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service",url = "${client.auth-service.url}")
 public interface AuthServiceClient {
 
     @PostMapping("/internal/api/auth/verify")
