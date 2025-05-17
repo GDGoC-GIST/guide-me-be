ed93c0df513ff2839817f5c3edadef5d74c91f8d
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2025-05-18 03:09:26 +0900

    fix(IMAGE-SERVICE): flyway수정 및 swagger 추가

--- image-service/src/main/java/guideme/imageservice/controller/ImageController.java
+++ image-service/src/main/java/guideme/imageservice/controller/ImageController.java
@@ -1,6 +1,8 @@
 package guideme.imageservice.controller;
 
 import guideme.imageservice.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +13,21 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(
+        name = "timetable",
+        description = "time table 업로드")
 @RequestMapping("/api/user/timetable")
 public class ImageController {
 
     private final ImageService imageService;
 
     @PostMapping("/upload")
+    @Operation(
+            summary = "사진 업로드",
+            description = "시간표 사진을 업로드하여 firebase로 올립니다"
+    )
     public ResponseEntity<Map<String, String>> upload(
-            @RequestParam("userId") String userId,
+            @RequestHeader("X-Client-Id") String userId,
             @RequestParam("file") MultipartFile file)
     {
         try {
--- image-service/src/main/java/guideme/imageservice/domain/UserRole.java
+++ image-service/src/main/java/guideme/imageservice/domain/UserRole.java
@@ -1,5 +1,10 @@
 package guideme.imageservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "유저 상태 코드", example = "ABLE")
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum UserRole {
     PENDING, ABLE, MASTER
 }
