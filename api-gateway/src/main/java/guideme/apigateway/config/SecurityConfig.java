f872224e7d91942ee84838da943ef1a497427570
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2025-05-18 00:48:51 +0900

    fix: swagger prefix, gateway url 수정

--- api-gateway/src/main/java/guideme/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/guideme/apigateway/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -21,11 +22,12 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/actuator/health/**").permitAll()
-                        .pathMatchers("/api/auth/**","/api/auth/**").permitAll()
+                        .pathMatchers("/api/auth/**","/api/user/**").permitAll()
                         .pathMatchers("/admin/**").hasRole("ADMIN")
                         .pathMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .anyExchange().authenticated()
                 )
+                .formLogin(FormLoginSpec::disable)
                 .authenticationManager(authManager)
                 .addFilterAfter(userIdInjectFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
