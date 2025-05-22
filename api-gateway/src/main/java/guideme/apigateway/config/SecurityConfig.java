80c6e903d69a5dc0f8660c29cb5589a6a7de99ff
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2025-05-22 18:45:57 +0900

    fix(GATEWAY): whitelist 수정

--- api-gateway/src/main/java/guideme/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/guideme/apigateway/config/SecurityConfig.java
@@ -8,28 +8,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.RequestPath;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final List<String> WHITE_LIST = List.of("/api/auth/**", "/api/user/v3/api-docs/**",
+            "/api/user/swagger-ui/**", "/actuator/health");
     private final CustomAuthenticationManager authManager;
-
-    private static final List<String> WHITE_LIST = List.of(
-            "/api/auth/",
-            "/api/public/",
-            "/api/user/v3/api-docs",
-            "/api/user/swagger-ui/index.html",
-            "/api/user/v3/api-docs/swagger-config",
-            "/actuator/health"
-    );
+    private final PathPatternParser parser = new PathPatternParser();
 
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, UserIdInjectFilter userIdInjectFilter) {
@@ -38,22 +34,23 @@ public class SecurityConfig {
         authenticationWebFilter.setServerAuthenticationConverter(new BearerTokenConverter());
         authenticationWebFilter.setSecurityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 
-        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges.pathMatchers("/actuator/health/**").permitAll().pathMatchers("/api/auth/**")
-                        .permitAll().pathMatchers("/api/user/v3/api-docs").permitAll()
-                        .pathMatchers("/api/user/swagger-ui/index.html").permitAll()
-                        .pathMatchers("/api/user/v3/api-docs/swagger-config").permitAll()
-                .anyExchange().authenticated())
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange(
+                        exchanges -> exchanges
+                                .pathMatchers("/actuator/health/**").permitAll()
+                                .pathMatchers("/api/auth/**").permitAll()
+                                .pathMatchers("/api/user/swagger-ui/**").permitAll()
+                                .pathMatchers("/api/user/v3/api-docs/**").permitAll()
+                                .anyExchange().authenticated())
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(FormLoginSpec::disable)
                 .addFilterAt((exchange, chain) -> {
-                    String path = exchange.getRequest().getPath().value();
-                    log.info(path);
-                    if (WHITE_LIST.stream().anyMatch(path::startsWith)) {
+                    RequestPath path = exchange.getRequest().getPath();
+                    if (WHITE_LIST.stream().anyMatch(p -> parser.parse(p).matches(path))) {
+                        log.info(path.value());
                         return chain.filter(exchange); // 인증 필터 스킵
                     }
                     return authenticationWebFilter.filter(exchange, chain);
                 }, SecurityWebFiltersOrder.AUTHENTICATION)
-                .addFilterAfter(userIdInjectFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-                .build();
+                .addFilterAfter(userIdInjectFilter, SecurityWebFiltersOrder.AUTHENTICATION).build();
     }
 }
